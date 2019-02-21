d08fe1cb36491ed6b423a8746bab164454ccdb53
Chris Davison <c.jr.davison@gmail.com>
2019-02-21 12:55:25 +0000

    Remove the concept of 'priority'

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate regex;
 use std::env;
 
 mod modify;
-mod priority;
 mod schedule;
 mod todo;
 mod utility;
@@ -22,23 +21,17 @@ COMMANDS:
     rm IDX                  Remove item IDX
     do IDX                  Move item IDX to $DONEFILE (done)
     undo IDX                Move item IDX from $DONEFILE into $TODOFILE
-
-    up IDX                  Upgrade task IDX to a priority
-    down IDX                Downgrade task IDX to a normal task
-
-    schedule IDX [DATE]     Schedule task IDX.  If no date, will prompt.
+    
+    schedule IDX [DATE]     Schedule task IDX.  If no DATE, will prompt.
     unschedule IDX          Remove due date from task IDX.
     today IDX               Schedule task IDX for today
 
-    // VIEW commands
-    ls                      Show tasks (optionally filtered)
-    lsp                     Show prioritised tasks (optionally filtered)
-    lsd                     Show done tasks (listdone)
-    mit                     Show overdue, or due today, prioritised tasks
-    due                     Show overdue, due today, and tasks due in NDAYS
-    nd                      Show all todos without a due date (nodate)
-
-    help                    Display this message
+    // VIEW...
+    ls                      ...tasks (optionally filtered)
+    lsd                     ...done tasks (listdone)
+    due                     ...scheduled tasks
+    nd                      ...unscheduled tasks
+    help                    ...this message
 ";
 
 type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
@@ -61,19 +54,15 @@ fn main() -> Result<()> {
         "undo" => modify::undo(&args),
         "app" | "append" => modify::append(&args),
         "pre" | "prepend" => modify::prepend(&args),
-        "up" | "upgrade" => priority::upgrade(&args),
-        "down" | "downgrade" => priority::downgrade(&args),
         "schedule" => schedule::schedule(&args),
         "unschedule" => schedule::unschedule(&args),
         "today" => schedule::today(&args),
         // ========== Filtered views
         "ls" | "list" => view::list(&todos, &args),
-        "lsp" => view::list_priorities(&todos, &args),
         "lsd" | "listdone" => view::done(&dones, &args),
         // ========== Date-based views
-        "due" => view::dated::due(&todos, &args),
-        "nd" | "nodate" => view::dated::no_date(&todos, &args),
-        "mit" | "important" => view::dated::mit(&todos, &args),
+        "due" => view::due(&todos, &args),
+        "nd" | "nodate" => view::no_date(&todos, &args),
         // ========== Utility
         _ => {
             println!("{}", USAGE);
--- src/view.rs
+++ src/view.rs
@@ -22,16 +22,6 @@ pub fn list(todos: &[todo::Todo], args: &[String]) -> Result<()> {
     Ok(())
 }
 
-pub fn list_priorities(todos: &[todo::Todo], args: &[String]) -> Result<()> {
-    let (todos, _) = utility::filter_todos(&todos, &args);
-    for t in todos {
-        if t.priority {
-            println!("{}", t);
-        }
-    }
-    Ok(())
-}
-
 pub fn done(todos: &[todo::Todo], args: &[String]) -> Result<()> {
     let (todos, _) = utility::filter_todos(&todos, &args);
     for t in todos {
@@ -40,45 +30,26 @@ pub fn done(todos: &[todo::Todo], args: &[String]) -> Result<()> {
     Ok(())
 }
 
-pub mod dated {
-    use super::*;
+pub fn due(todos: &[todo::Todo], args: &[String]) -> Result<()> {
+    let (todos, _args) = utility::filter_todos(&todos, &args);
+    let mut todos_with_datediff: Vec<(i64, todo::Todo)> = todos
+        .iter()
+        .filter(|x| x.date != "")
+        .map(|x| (get_datediff(x).unwrap(), x.to_owned()))
+        .collect();
+    todos_with_datediff.sort_by(|a, b| b.0.cmp(&a.0));
+    for (_diff, t) in todos_with_datediff {
+        println!("{}", t);
+    }
+    Ok(())
+}
 
-    pub fn due(todos: &[todo::Todo], args: &[String]) -> Result<()> {
-        let (todos, _args) = utility::filter_todos(&todos, &args);
-        let mut todos_with_datediff: Vec<(i64, todo::Todo)> = todos
-            .iter()
-            .filter(|x| x.date != "")
-            .map(|x| (get_datediff(x).unwrap(), x.to_owned()))
-            .collect();
-        todos_with_datediff.sort_by(|a, b| b.0.cmp(&a.0));
-        for (_diff, t) in todos_with_datediff {
+pub fn no_date(todos: &[todo::Todo], args: &[String]) -> Result<()> {
+    let (todos, _args) = utility::filter_todos(&todos, &args);
+    for t in todos {
+        if t.date == "" {
             println!("{}", t);
         }
-        Ok(())
     }
-
-    pub fn mit(todos: &[todo::Todo], args: &[String]) -> Result<()> {
-        let (todos, _args) = utility::filter_todos(&todos, &args);
-        let mut todos_with_datediff: Vec<(i64, todo::Todo)> = todos
-            .iter()
-            .filter(|x| x.date != "" && x.priority)
-            .map(|x| (get_datediff(x).unwrap(), x.to_owned()))
-            .filter(|(i, _x)| *i >= 0)
-            .collect();
-        todos_with_datediff.sort_by(|a, b| b.0.cmp(&a.0));
-        for (_diff, t) in todos_with_datediff {
-            println!("{}", t);
-        }
-        Ok(())
-    }
-
-    pub fn no_date(todos: &[todo::Todo], args: &[String]) -> Result<()> {
-        let (todos, _args) = utility::filter_todos(&todos, &args);
-        for t in todos {
-            if t.date == "" {
-                println!("{}", t);
-            }
-        }
-        Ok(())
-    }
+    Ok(())
 }
